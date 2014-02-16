ce2886650df13ffdf1ebe211fd0dfdb3c86f58d9
Arild Nilsen <arild.nilsen2@gmail.com>
2014-02-16 19:01:13 +0100

    Mock compute actor factory

--- src/test/scala/ComputeSupervisorTest.scala
+++ src/test/scala/ComputeSupervisorTest.scala
@@ -1,12 +1,17 @@
-import akka.actor.{ActorRef, Props, ActorSystem}
+import akka.actor.{ActorContext, Props, ActorRef, ActorSystem}
 import akka.testkit.{TestActorRef, ImplicitSender, TestKit}
-import org.scalatest.{BeforeAndAfterAll, FunSpecLike}
 import scala.concurrent.duration._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{Matchers, BeforeAndAfterAll, FunSpecLike}
+import org.mockito.Matchers._
+import org.mockito.Mockito._
 
 class ComputeSupervisorTest(_system: ActorSystem) extends TestKit(_system)
 with FunSpecLike
 with ImplicitSender
-with BeforeAndAfterAll {
+with BeforeAndAfterAll
+with MockitoSugar
+with Matchers {
 
   def this() = this(ActorSystem("testSystem"))
 
@@ -14,14 +19,19 @@ with BeforeAndAfterAll {
     TestKit.shutdownActorSystem(system)
   }
 
-  it("should start ComputeActor and return an ActorRef") {
-    val computeSupervisor = TestActorRef(Props(new ComputeSupervisor(new ComputeActorFactory)))
+  it("should start ComputeActor and return ActorRef") {
 
-    computeSupervisor ! StartComputeActor("compueActor1")
+    val computeActorFactory = mock[ComputeActorFactory]
+    val computeActor: ActorRef = mock[ActorRef]
+    when(computeActorFactory.create(any[ActorContext], anyString())).thenReturn(computeActor)
+
+    val computeSupervisor = TestActorRef(Props(new ComputeSupervisor(computeActorFactory)))
+
+    computeSupervisor ! StartComputeActor("computeActor-1")
 
     within(500 millis){
-      expectMsgClass(classOf[ActorRef])
+      val actor: ActorRef = expectMsgClass(classOf[ActorRef])
+      actor should equal(computeActor)
     }
-
   }
 }
