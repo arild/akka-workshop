a5761c1d348ff4e8a19aa7475faf5b43117ec7df
SjurMillidahl <sjur.millidahl@bekk.no>
2014-03-02 18:31:15 +0100

    Slides with instructions for part I

--- src/main/scala/workshop/GreetingActor.scala
+++ src/main/scala/workshop/GreetingActor.scala
@@ -2,12 +2,16 @@ package workshop
 
 import akka.actor.{Props, ActorSystem, Actor}
 
+case class MyCommand()
+
 class GreetingActor extends Actor {
   def receive = {
-    case message : String ⇒ println("Hello " + message)
+    case message : String => println("Hello " + message)
+    case command : MyCommand => println("Got a command!")
   }
 
   val system = ActorSystem("MySystem")
   val greeter = system.actorOf(Props[GreetingActor], name = "greeter")
   greeter ! "Charlie Parker"
+  greeter ! MyCommand
 }
