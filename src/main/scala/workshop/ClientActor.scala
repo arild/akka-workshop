0750e4570e33097f2fc01f2e08415992b62e3673
mariatsji <sjur.millidahl@gmail.com>
2014-03-09 19:10:01 +0100

    Removed ClientActor implementation

--- src/main/scala/workshop/ClientActor.scala
+++ src/main/scala/workshop/ClientActor.scala
@@ -8,21 +8,8 @@ import workshop.work.{RiskyWorkResult, RiskyWork}
 class ClientActor(computeSupervisor: ActorRef, resultActor: ActorRef, work: List[RiskyWork]) extends Actor {
   val log = Logging(context.system, this)
 
-  override def preStart() = {
-    computeSupervisor ! StartComputeActor("computeActor")
-  }
-
   def receive = {
-    case computeActor: ActorRef => {
-      context.watch(computeActor)
-      work.foreach(w => computeActor ! w)
-    }
-    case result: RiskyWorkResult => {
-      resultActor ! result
-    }
-    case Terminated => {
-      log.error("Compute actor terminated, terminating self")
-      context.stop(self)
-    }
+    //TODO
+    case _ => {}
   }
 }
