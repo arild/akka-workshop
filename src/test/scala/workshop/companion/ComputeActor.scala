5b7d4596514744890030dcaed0d861930f5f699c
mariatsji <sjur.millidahl@gmail.com>
2014-05-18 14:23:49 +0200

    fjernet warns

--- src/test/scala/workshop/companion/ComputeActor.scala
+++ src/test/scala/workshop/companion/ComputeActor.scala
@@ -2,9 +2,8 @@ package workshop.companion
 
 import scala.concurrent.duration.FiniteDuration
 import akka.actor.Props
-import workshop.ComputeActor
 
 // Companion object would usually be located in same file as class
 object ComputeActor {
-  def props(logCompletedTasksInterval: FiniteDuration): Props = Props(new ComputeActor(logCompletedTasksInterval))
+  def props(logCompletedTasksInterval: FiniteDuration): Props = Props(new workshop.ComputeActor(logCompletedTasksInterval))
 }
