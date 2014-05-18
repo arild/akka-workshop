6574f2d35512737ddcbe9c6de560742ccd9f371d
mariatsji <sjur.millidahl@gmail.com>
2014-05-18 14:23:49 +0200

    fjernet warns

--- src/test/scala/workshop/companion/ComputeSupervisor.scala
+++ src/test/scala/workshop/companion/ComputeSupervisor.scala
@@ -1,9 +1,8 @@
 package workshop.companion
 
-import workshop.{ComputeSupervisor, ComputeActorFactory}
 import akka.actor.Props
 
 // Companion object would usually be located in same file as class
 object ComputeSupervisor {
-  def props(computeActorFactory: ComputeActorFactory): Props = Props(new ComputeSupervisor(computeActorFactory))
+  def props(computeActorFactory: workshop.ComputeActorFactory): Props = Props(new workshop.ComputeSupervisor(computeActorFactory))
 }
