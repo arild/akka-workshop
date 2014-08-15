67d45ba29858d157b0445806862fb18bd314e435
Arild Nilsen <arild.nilsen2@gmail.com>
2014-08-15 16:51:56 +0200

    Router impl similar to scala

--- src/main/java/workshop/part3/SuperComputeRouter.java
+++ src/main/java/workshop/part3/SuperComputeRouter.java
@@ -1,37 +1,28 @@
 package workshop.part3;
 
 import akka.actor.*;
-import akka.event.Logging;
-import akka.event.LoggingAdapter;
 import akka.japi.pf.ReceiveBuilder;
-import akka.routing.*;
+import akka.routing.DefaultResizer;
+import akka.routing.RoundRobinPool;
 import scala.PartialFunction;
 import scala.concurrent.duration.Duration;
 import scala.runtime.BoxedUnit;
-import workshop.examples.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static akka.actor.SupervisorStrategy.resume;
 import static work.Work.RiskyWork;
 
 public class SuperComputeRouter extends AbstractActor {
 
+   private RoundRobinPool roundRobinPool = new RoundRobinPool(10)
+           .withResizer(new DefaultResizer(5, 20));
+
+    ActorRef router =
+            getContext().actorOf(roundRobinPool.props(Props.create(Worker.class)),
+                    "router-1");
+
     private SupervisorStrategy strategy =
             new OneForOneStrategy(10, Duration.create("1 minute"),
                     t -> resume(), false);
-
-    Router router;
-    {
-        List<Routee> routees = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            ActorRef r = context().actorOf(Props.create(Worker.class));
-            context().watch(r);
-            routees.add(new ActorRefRoutee(r));
-        }
-        router = new Router(new RoundRobinRoutingLogic(), routees);
-    }
 
     @Override
     public SupervisorStrategy supervisorStrategy() {
@@ -41,7 +32,7 @@ public class SuperComputeRouter extends AbstractActor {
     @Override
     public PartialFunction<Object, BoxedUnit> receive() {
         return ReceiveBuilder
-                .match(RiskyWork.class, w -> router.route(w, sender()))
+                .match(RiskyWork.class, w -> router.tell(w, sender()))
                 .build();
     }
 
