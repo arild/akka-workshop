f6aa0890635ca1e6f7341881a052a0004704d5cd
Arild Nilsen <arild.nilsen2@gmail.com>
2015-03-27 09:38:56 +0100

    Delete unused companion object

--- src/test/scala/workshop/companion/ComputeSupervisor.scala
+++
@@ -1,8 +0,0 @@
-package workshop.companion
-
-import akka.actor.Props
-
-// Companion object would usually be located in same file as class
-object ComputeSupervisor {
-  def props(computeActorFactory: workshop.part2.ComputeActorFactory): Props = Props(new workshop.part2.ComputeSupervisor(computeActorFactory))
-}
